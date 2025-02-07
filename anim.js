9fa9e8d32b1d09ed0c1b3b7052b9364b1a339f40
soyhuta <doruckgg@gmail.com>
2025-02-07 13:32:16 -0500

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,55 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "Oh, I never knew you were the someone", time: 1 },
+  { text: "waitin' for me", time: 5 },
+  { text: "Cause we were just kids when we fell in love", time: 8 },
+  { text: "not knowin' what it was", time: 12 },
+  { text: "I will not give you up", time: 16 },
+  { text: "this time", time: 20 },
+  { text: "Oh, darling, just kiss me slow", time: 24 },
+  { text: "your heart is all I own", time: 28 },
+  { text: "And in your eyes", time: 31 },
+  { text: "you're holding mine", time: 33 },
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 4
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
